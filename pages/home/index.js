83b5e4f9fb51b506605d986f37b607b5b55c8d61
vitorfront <vitorrventurini2004@hotmail.com>
2023-06-11 12:30:20 -0300

    feat: implemente data on table

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,3 +1,55 @@
+const renderfinancesList = (data) => {
+  const table = document.getElementById("finances-table");
+
+  data.map((item) => {
+    const tableRow = document.createElement("tr");
+    tableRow.className = "mt smaller";
+
+    //  title
+    const titleTd = document.createElement("td");
+    const titleText = document.createTextNode(item.title);
+    titleTd.appendChild(titleText);
+    tableRow.appendChild(titleTd);
+
+    //  category
+    const categoryTd = document.createElement("td");
+    const categoryText = document.createTextNode(item.name);
+    categoryTd.appendChild(categoryText);
+    tableRow.appendChild(categoryTd);
+
+    //  category
+    const dateTd = document.createElement("td");
+    const dateText = document.createTextNode(
+      new Date(item.date).toLocaleDateString()
+    );
+    dateTd.appendChild(dateText);
+    tableRow.appendChild(dateTd);
+
+    //  value
+    const valueTd = document.createElement("td");
+    valueTd.className = "center";
+    const valueText = document.createTextNode(
+      new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      }).format(item.value)
+    );
+    valueTd.appendChild(valueText);
+    tableRow.appendChild(valueTd);
+
+    // delete
+
+    const deleteTd = document.createElement("td");
+    deleteTd.className = "right";
+    const deleteText = document.createTextNode("Deletar");
+    deleteTd.appendChild(deleteText);
+    tableRow.appendChild(deleteTd);
+
+    // table add tablerow
+    table.appendChild(tableRow);
+  });
+};
+
 const renderFinancesElements = (data) => {
   const totalItems = data.lenght;
   const revenues = data
@@ -72,6 +124,7 @@ const onLoadFinancesData = async () => {
     );
     const data = await result.json();
     renderFinancesElements(data);
+    renderfinancesList(data);
     return data;
   } catch (error) {
     return { error };
